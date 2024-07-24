26ce9942efa168ebeb0ea0a864979928816d3abe
Eric P. <56278025+CodeYut@users.noreply.github.com>
2024-07-24 10:43:10 -0700

    Update CanPlaceFlowers.cs

--- CanPlaceFlowers.cs
+++ CanPlaceFlowers.cs
@@ -5,16 +5,16 @@
 
 public class Solution {
     public bool CanPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0;
+        int count = 0;  //initialize the integer count to 0.
 
-        for (int i = 0; i < flowerbed.Length; i++)
+        for (int i = 0; i < flowerbed.Length; i++)  //iterate through the length of the array.
         {
-            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) {
-                flowerbed[i] = 1;
-                count++;
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) {  //checking if the flowerbed array equals 0 or other conditions.
+                flowerbed[i] = 1;  //making the flowerbed array equal to 1.
+                count++;  //increase the count by 1.
             }
         }
 
-        return count >= n;
+        return count >= n;  //returning the count greater or equal to n.  returns a bool from the comparison.
     }
 }
