043fe3a6e8cc399c7f6c3838269cc571a42468a3
Eric P. <56278025+CodeYut@users.noreply.github.com>
2024-07-24 19:30:18 -0700

    Update ReverseVowelsofaString.cs

--- ReverseVowelsofaString.cs
+++ ReverseVowelsofaString.cs
@@ -3,24 +3,24 @@
 
 public class Solution {
     public string ReverseVowels(string s) {
-        Stack<char> vowelsInTheString = new Stack<char>();
-        HashSet<char> vowels = new HashSet<char>() {'a', 'e', 'o', 'u', 'i', 'A', 'E', 'O', 'U', 'I'};
-        HashSet<int> indexesOfVowels = new HashSet<int>();
-        StringBuilder result = new StringBuilder(s);
+        Stack<char> vowelsInTheString = new Stack<char>();  //create a new stack.
+        HashSet<char> vowels = new HashSet<char>() {'a', 'e', 'o', 'u', 'i', 'A', 'E', 'O', 'U', 'I'};  //create a hashset including the vowels.
+        HashSet<int> indexesOfVowels = new HashSet<int>();  //This hashset is for the integer index of the vowels.
+        StringBuilder result = new StringBuilder(s);  //create a new StringBuilder.
 
-        for (var i = 0; i < s.Length; i++) {
-            if(vowels.Contains(s[i])) {
-                vowelsInTheString.Push(s[i]);
-                indexesOfVowels.Add(i);
+        for (var i = 0; i < s.Length; i++) {  //iterate through the entire array.
+            if(vowels.Contains(s[i])) {   //if the vowels hashset contains the string array.
+                vowelsInTheString.Push(s[i]);  //push the string array position onto the stack.
+                indexesOfVowels.Add(i);  //add the letter to the hashset.
             }
         }
 
-        for(var i = 0; i < s.Length; i++) {
-            if(indexesOfVowels.Contains(i)) {
-                result[i] = vowelsInTheString.Pop();
+        for(var i = 0; i < s.Length; i++) {  //iterate through the entire array again.
+            if(indexesOfVowels.Contains(i)) {  //check if the hashset contains the letter.
+                result[i] = vowelsInTheString.Pop();  //take the result array and set it equal to the hashset after the pop happens.
             }
         }
 
-        return result.ToString();
+        return result.ToString();  //return the result to a string.
     }
 }
